b4936873160d9617346b4b9dd0ec2b9f3c36ef6e
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2025-02-25 18:22:21 -0300

    feat: create and add new route API - CreateCategory

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,13 +3,22 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
+//-- User --
 router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthUserController().handle);
 
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+//-- Category --
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
