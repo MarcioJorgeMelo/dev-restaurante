a5935a6cac159017cdec089b4fda818b0f12d80f
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-03-10 13:32:15 -0300

    feat: add cookies to set acesstoken

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,56 @@
+import { api } from "@/services/api";
 import styles from "./page.module.scss";
 import logoImg from "/public/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export default function Page() {
+  async function handleLogin(formData: FormData) {
+    "use server";
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (email === "" || password === "") {
+      return;
+    }
+
+    try {
+      const response = await api.post("/session", {
+        email,
+        password,
+      });
+
+      if (!response.data.token) {
+        return;
+      }
+
+      const expressTime = 60 * 60 * 24 * 30 * 1000;
+      const cookieStore = await cookies();
+
+      cookieStore.set("session", response.data.token, {
+        maxAge: expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production",
+      });
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+
+    redirect("/dashboard");
+  }
+
   return (
     <>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo da pizzaria" />
 
         <section className={styles.login}>
-          <form>
+          <form action={handleLogin}>
             <input
               type="email"
               required
