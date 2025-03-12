6f7115f7c1f1d62259a64a7066a0c905febaf049
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-03-12 16:26:55 -0300

    feat: create visual modalOrder component

---
+++ frontend/src/app/dashboard/components/modal/index.tsx
@@ -0,0 +1,33 @@
+import { X } from "lucide-react";
+import styles from "./styles.module.scss";
+
+export function ModalOrder() {
+  return (
+    <dialog className={styles.dialogContainer}>
+      <section className={styles.dialogContent}>
+        <button className={styles.dialogBack}>
+          <X size={40} color="#FF3f4b" />
+        </button>
+
+        <article className={styles.container}>
+          <h2>Detalhes do pedido</h2>
+
+          <span className={styles.table}>
+            Mesa <b>36</b>
+          </span>
+
+          <section className={styles.item}>
+            <span>
+              1 - <b>Pizza calabresa</b>
+            </span>
+            <span className={styles.description}>
+              Pizza de calabresa com borda recheada
+            </span>
+          </section>
+
+          <button className={styles.buttonOrder}>Concluir o pedido</button>
+        </article>
+      </section>
+    </dialog>
+  );
+}
