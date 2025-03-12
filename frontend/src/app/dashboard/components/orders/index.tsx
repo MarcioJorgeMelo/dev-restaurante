6b7388387f378cf208099fd779c48c6f7d42f5ae
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-03-12 16:27:15 -0300

    feat: use modalOrder on Order component

--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -1,6 +1,7 @@
 import { OrderProps } from "@/lib/order.type";
 import styles from "./styles.module.scss";
 import { RefreshCcw } from "lucide-react";
+import { ModalOrder } from "../modal";
 
 interface OrderComponentProps {
   orders: OrderProps[];
@@ -8,27 +9,31 @@ interface OrderComponentProps {
 
 export function Orders({ orders }: OrderComponentProps) {
   return (
-    <main className={styles.container}>
-      <section className={styles.containerHeader}>
-        <h1>Últimos pedidos</h1>
+    <>
+      <main className={styles.container}>
+        <section className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
 
-        <button>
-          <RefreshCcw size={24} color="#3fffa3" />
-        </button>
-      </section>
+          <button>
+            <RefreshCcw size={24} color="#3fffa3" />
+          </button>
+        </section>
 
-      <section className={styles.listOrders}>
-        {orders ? (
-          orders.map((order) => (
-            <button key={order.id} className={styles.orderItem}>
-              <div className={styles.tag} />
-              <span>Mesa {order.table}</span>
-            </button>
-          ))
-        ) : (
-          <p>Nenhum pedido em aberto no momento : (</p>
-        )}
-      </section>
-    </main>
+        <section className={styles.listOrders}>
+          {orders ? (
+            orders.map((order) => (
+              <button key={order.id} className={styles.orderItem}>
+                <div className={styles.tag} />
+                <span>Mesa {order.table}</span>
+              </button>
+            ))
+          ) : (
+            <p>Nenhum pedido em aberto no momento : (</p>
+          )}
+        </section>
+      </main>
+
+      <ModalOrder />
+    </>
   );
 }
