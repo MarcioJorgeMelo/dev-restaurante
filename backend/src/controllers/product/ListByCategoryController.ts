a8004483741f841fc539c537ec59ffc2f9d34553
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-03-20 18:10:33 -0300

    feat: project deploy

--- backend/src/controllers/product/ListByCategoryController.ts
+++ backend/src/controllers/product/ListByCategoryController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { ListByCategoryService } from "../../services/product/listByCategoryService";
+import { ListByCategoryService } from "../../services/product/ListByCategoryService";
 
 class ListByCategoryControler {
   async handle(req: Request, res: Response) {
