2e272e016f6d542ca7d3ede34da2e02a0a43e0f8
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-03-24 14:10:54 -0300

    feat: create function to loading´s auth´s process and use global variable´s context

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
+import { View, ActivityIndicator } from "react-native";
+
+import { AuthContext } from "../contexts/AuthContext";
 
 function Routes() {
-  const isAuthenticated = false;
-  const loading = false;
+  const { isAuthenticated, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#1D1D2E",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size={60} color="#F5f7fb" />
+      </View>
+    );
+  }
 
   return isAuthenticated ? <AppRoutes /> : <AuthRoutes />;
 }
