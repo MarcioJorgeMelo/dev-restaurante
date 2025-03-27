4fe4a4e357fa0a017096e38f610647adc5998e83
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-03-27 08:54:45 -0300

    feat: create itens list with all funcionality with API´s calls and 'fix' to add onPress to open modalProduct

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   TextInput,
   Modal,
+  FlatList,
 } from "react-native";
 
 import { useRoute, RouteProp, useNavigation } from "@react-navigation/native";
@@ -13,6 +14,7 @@ import { useRoute, RouteProp, useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
 import { api } from "../../services/api";
 import { ModalPicker } from "../../components/ModalPicker";
+import { ListItem } from "../../components/ListItem";
 
 type RouteDetailParams = {
   Order: {
@@ -29,6 +31,13 @@ export type CategoryProps = {
 type ProductProps = {
   id: string;
   name: string;
+};
+
+type ItemProps = {
+  id: string;
+  product_id: string;
+  name: string;
+  amount: string | number;
 };
 
 type OrderRouteProps = RouteProp<RouteDetailParams, "Order">;
@@ -51,6 +60,8 @@ export default function Order() {
   const [modalProductVisible, setModalProductVisible] = useState(false);
 
   const [amount, setAmount] = useState("1");
+
+  const [items, setItems] = useState<ItemProps[]>([]);
 
   useEffect(() => {
     async function loadInfo() {
@@ -110,13 +121,55 @@ export default function Order() {
     setProductSelected(item);
   }
 
+  async function handleAddItem() {
+    try {
+      const response = await api.post("/order/add", {
+        order_id: route.params?.order_id,
+        product_id: productSelected?.id,
+        amount: Number(amount),
+      });
+
+      let data = {
+        id: response.data.id,
+        product_id: productSelected?.id as string,
+        name: productSelected?.name as string,
+        amount: amount,
+      };
+
+      setItems((prev) => [...prev, data]);
+    } catch (error) {
+      console.log("Erro ao adicionar um item ao pedido ", error);
+    }
+  }
+
+  async function handleDeleteItem(item_id: string) {
+    try {
+      const response = await api.delete("order/remove", {
+        params: {
+          item_id: item_id,
+        },
+      });
+
+      let removeItem = items.filter((item) => {
+        return item.id !== item_id;
+      });
+
+      setItems(removeItem);
+    } catch (error) {
+      console.log("Erro ao deletar item do pedido ", error);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Mesa {route.params.number}</Text>
-        <TouchableOpacity onPress={handleCloseOrder}>
-          <Feather name="trash-2" size={28} color="#FF3F4b" />
-        </TouchableOpacity>
+
+        {items.length === 0 && (
+          <TouchableOpacity onPress={handleCloseOrder}>
+            <Feather name="trash-2" size={28} color="#FF3F4b" />
+          </TouchableOpacity>
+        )}
       </View>
 
       {category.length !== 0 && (
@@ -129,7 +182,10 @@ export default function Order() {
       )}
 
       {products.length !== 0 && (
-        <TouchableOpacity style={styles.input}>
+        <TouchableOpacity
+          style={styles.input}
+          onPress={() => setModalProductVisible(true)}
+        >
           <Text style={{ color: "#FFF" }}>{productSelected?.name}</Text>
         </TouchableOpacity>
       )}
@@ -147,14 +203,27 @@ export default function Order() {
       </View>
 
       <View style={styles.actions}>
-        <TouchableOpacity style={styles.buttonAdd}>
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAddItem}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={[styles.button, { opacity: items.length === 0 ? 0.3 : 1 }]}
+          disabled={items.length === 0}
+        >
           <Text style={styles.buttonText}>Avançar</Text>
         </TouchableOpacity>
       </View>
+
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        style={{ flex: 1, marginTop: 24 }}
+        data={items}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ListItem data={item} deleteItem={handleDeleteItem} />
+        )}
+      />
 
       <Modal
         transparent={true}
